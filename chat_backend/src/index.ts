3fac9ff15dcc349db96d59feee2a868ba8588dc0
samnart1 <originalsamnart@gmail.com>
2024-12-04 00:20:21 +0100

    neccessary backend crud operations complete

--- chat_backend/src/index.ts
+++ chat_backend/src/index.ts
@@ -1,19 +1,6 @@
-import { Hono } from 'hono'
-import { logger } from 'hono/logger'
-import { timing } from 'hono/timing'
-// import dotenv from 'dotenv'
-// dotenv.config({ path: '.env' })
-// dotenv.config({ path: '.env.dev' })
+// ten
+import { createInMemoryApp } from './controllers/main'
 
-const app = new Hono()
-app.use('*', timing())
-app.use('*', logger())
-
-console.log(Bun.env.TEST)
-console.log(Bun.env.AI)
-
-app.get('/', (c) => {
-  return c.json({ message: 'Hello Hono!' })
-})
+const app = createInMemoryApp()
 
 export default app
