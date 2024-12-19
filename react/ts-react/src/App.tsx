bf779bc590e91fba1c2735f2ac02d0710bdf81ab
samnart1 <originalsamnart@gmail.com>
2024-12-19 23:41:52 +0100

    work from yesterday

--- react/ts-react/src/App.tsx
+++ react/ts-react/src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css'
-import Component from './01-return'
+// import Component from './01-return'
+import Component from './04-events'
 
 function App() {
   return (
     <>
-      <Component >
-        <h1>Hello World</h1>
-      </Component>
-      <Component name="Sam" id={1234} />
+      <Component />
     </>
   )
 }
