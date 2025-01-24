a55b8c9b16b3bebc554adde79f9cd1afb7301331
samnart1 <originalsamnart@gmail.com>
2025-01-24 03:13:49 +0100

    linkedlists

--- datastructures/Linkedlists.ts
+++ datastructures/Linkedlists.ts
@@ -15,14 +15,46 @@ class LinkedList<T> {
     this.head = null;
   }
 
-  append(data: T): void {
-    const current = new NNode(data);
+  append(data: T) {
+    const newNode = new NNode(data);
 
     if (!this.head) {
-        this.head = current;
-        return;
+      this.head = newNode;
+      return;
+    }
+    
+    let current = this.head;
+
+    while (current.next) {
+      current = current.next;
     }
 
-    
+    current.next = newNode;
+  }
+
+  printList() {
+    if (!this.head) {
+      console.log("THe list is empty.");
+      return;
+    }
+
+    let current: NNode<T> | null = this.head;
+    const elements: T[] = [];
+
+    while (current) {
+      elements.push(current.data);
+      current = current.next;
+    }
+
+    console.log(elements.join(" -> "));
   }
 }
+
+
+const list = new LinkedList<number>();
+list.append(10);
+list.append(20);
+list.append(30);
+list.append(40);
+list.append(50);
+list.printList();
