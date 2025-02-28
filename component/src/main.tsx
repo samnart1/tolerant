23184fb03ac8372a376aac9e486cec96d0aaf42a
samnart1 <originalsamnart@gmail.com>
2025-02-28 19:36:41 +0100

    small changes

--- component/src/main.tsx
+++ component/src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 // import './index.css'
 // import App from './App.tsx'
 // import Counter from './Counter.tsx'
-import Effect from './Effect'
+import Effect from "./Effect";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* <App /> */}
     <Effect />
-  </StrictMode>,
-)
+  </StrictMode>
+);
