0358f94fa23c29dc54d56bf9ffdd3ada4624cb5c
samnart1 <originalsamnart@gmail.com>
2025-03-01 06:01:39 +0100

    practiced useRef Hook and custom hooks in react

--- component/src/main.tsx
+++ component/src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 // import './index.css'
 // import App from './App.tsx'
 // import Counter from './Counter.tsx'
-import Effect from "./Effect";
+// import Effect from "./Effect";
+import UseRef from "./UseRef";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* <App /> */}
-    <Effect />
+    {/* <Effect /> */}
+    <UseRef />
   </StrictMode>
 );
