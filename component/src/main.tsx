8a58d500c64263d1b8a25e14f0325c2cc3c5afb7
samnart1 <originalsamnart@gmail.com>
2025-03-03 21:13:48 +0100

    working with react, api's and axios

--- component/src/main.tsx
+++ component/src/main.tsx
@@ -1,17 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import './index.css'
-// import App from './App.tsx'
-// import Counter from './Counter.tsx'
-// import Effect from "./Effect";
-// import UseRef from "./UseRef";
-import Drill from "./Drill";
+// import API from "./API";
+import APIPost from "./APIPost"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <App /> */}
-    {/* <Effect /> */}
-    {/* <UseRef /> */}
-    <Drill />
+    {/* <API /> */}
+    <APIPost />
   </StrictMode>
 );
