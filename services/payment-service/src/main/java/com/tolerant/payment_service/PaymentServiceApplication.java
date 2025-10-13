1bc122f39ff7f00e680fa534ca2d10086d4a0ea1
samnart1 <originalsamnart@gmail.com>
2025-10-14 01:20:44 +0200

    chore: files

--- services/payment-service/src/main/java/com/tolerant/payment_service/PaymentServiceApplication.java
+++ services/payment-service/src/main/java/com/tolerant/payment_service/PaymentServiceApplication.java
@@ -2,12 +2,16 @@ package com.tolerant.payment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentServiceApplication.class, args);
+
+		System.out.println("Payment Service Started Successfully on port: 8082");
 	}
 
 }
