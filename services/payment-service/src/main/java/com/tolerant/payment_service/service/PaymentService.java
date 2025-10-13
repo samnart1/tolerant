b3fbe087edf18ea8ee244a2f16322b94ec425403
samnart1 <originalsamnart@gmail.com>
2025-10-14 01:20:07 +0200

    chore: add services

--- services/payment-service/src/main/java/com/tolerant/payment_service/service/PaymentService.java
+++ services/payment-service/src/main/java/com/tolerant/payment_service/service/PaymentService.java
@@ -1,5 +1,16 @@
 package com.tolerant.payment_service.service;
 
-public class PaymentService {
-    
+import java.util.List;
+
+import com.tolerant.payment_service.model.Payment;
+import com.tolerant.payment_service.model.PaymentRequest;
+import com.tolerant.payment_service.model.PaymentResponse;
+
+public interface PaymentService {
+    PaymentResponse processPayment(PaymentRequest request);
+    Payment getPayment(Long id);
+    Payment getPaymentByPaymentId(String paymentId);
+    Payment getPaymentByOrderId(String orderId);
+    List<Payment> getAllPayments();
+    List<Payment> getPaymentsByCustomer(String customerId);
 }
