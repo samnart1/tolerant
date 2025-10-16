d1d6fa20d41a4c851a137edd6c29c4a051c2d9db
samnart1 <originalsamnart@gmail.com>
2025-10-16 05:33:47 +0200

    chore: modify notification application

--- services/notification-service/src/main/java/com/tolerant/notification_service/NotificationServiceApplication.java
+++ services/notification-service/src/main/java/com/tolerant/notification_service/NotificationServiceApplication.java
@@ -2,12 +2,16 @@ package com.tolerant.notification_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
+
+		System.out.println("Notification service running on port: 8084");
 	}
 
 }
