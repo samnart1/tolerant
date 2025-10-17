9ad5150f50e36d8426534f98f4c120fcdbf0aa74
samnart1 <originalsamnart@gmail.com>
2025-10-17 11:49:01 +0200

    chore: afafd

--- services/api-gateway/src/main/java/com/tolerant/api_gateway/ApiGatewayApplication.java
+++ services/api-gateway/src/main/java/com/tolerant/api_gateway/ApiGatewayApplication.java
@@ -2,12 +2,16 @@ package com.tolerant.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+
+		System.out.println("API Gateway up and running on port: 8761");
 	}
 
 }
