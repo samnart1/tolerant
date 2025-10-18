1140ebae93b531dbc0deebdb31c2cc80580abd1d
samnart1 <originalsamnart@gmail.com>
2025-10-18 08:17:33 +0200

    chore: fix circular dependency

--- services/notification-service/src/main/java/com/tolerant/notification_service/model/Notification.java
+++ services/notification-service/src/main/java/com/tolerant/notification_service/model/Notification.java
@@ -17,34 +17,34 @@ public class Notification {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    // @Column(nullable = false)
     private String notificationId;
 
-    @Column(nullable = false)
+    // @Column(nullable = false)
     private String orderId;
 
-    @Column(nullable = false)
+    // @Column(nullable = false)
     private String customerId;
 
     private String email;
 
     private String phone;
 
-    @Column(nullable = false, length = 1000)
+    // @Column(nullable = false, length = 1000)
     private String message;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    // @Column(nullable = false)
     private NotificationType type;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    // @Column(nullable = false)
     private NotificationStatus status;
 
-    @Column(nullable = false)
+    // @Column(nullable = false)
     private String channel; // for http or rabbitmq communication
 
-    @Column(nullable = false)
+    // @Column(nullable = false)
     private String failureReason;
 
     // @Column(nullable = false)
